a560a3f3a66cf7f4c55765e90125ee728c5a7b67
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-04-13 12:50:55 +0300

    added tests for calculator too

--- main.js
+++ main.js
@@ -14,7 +14,38 @@ function reverseString(string) {
   });
   return new_string;
 }
+
+const calculator = {
+  add(a, b) {
+    let res = +a + +b;
+    return this.formatResult(res);
+  },
+
+  sub(a, b) {
+    let res = +a - +b;
+    return this.formatResult(res);
+  },
+
+  mul(a, b) {
+    let res = +a * +b;
+    return this.formatResult(res);
+  },
+
+  div(a, b) {
+    let res = +a / +b;
+    return this.formatResult(res);
+  },
+
+  formatResult(result) {
+    if (result % 1 !== 0) {
+      result = result.toFixed(4).replace(/\.?0+$/, "");
+    }
+    return result;
+  },
+};
+
 module.exports = {
   capitalize: capitalize,
   reverseString: reverseString,
+  calculator: calculator,
 };
--- main.test.js
+++ main.test.js
@@ -1,4 +1,5 @@
-const { capitalize, reverseString } = require("./main");
+const { beforeEach } = require("node:test");
+const { capitalize, reverseString, calculator } = require("./main");
 
 describe("My tests", () => {
   test("capitalize works correctly", () => {
@@ -16,5 +17,31 @@ describe("My tests", () => {
     expect(str1).toBe("rac ecar");
     expect(str2).toBe("olleh");
     expect(str3).toBe("?tseT a si sihT");
+  });
+
+  describe("Calculator works correctly", () => {
+    test("Addition works", () => {
+      expect(calculator.add(2, 1)).toBe(3);
+      expect(calculator.add(2, -3)).toBe(-1);
+      expect(calculator.add(-4, -8)).toBe(-12);
+    });
+
+    test("Subtraction works", () => {
+      expect(calculator.sub(2, 1)).toBe(1);
+      expect(calculator.sub(2, -3)).toBe(5);
+      expect(calculator.sub(-4, -8)).toBe(4);
+    });
+
+    test("Multiplication works", () => {
+      expect(calculator.mul(2, 1)).toBe(2);
+      expect(calculator.mul(2, -3)).toBe(-6);
+      expect(calculator.mul(-4, -8)).toBe(32);
+    });
+
+    test("Division works", () => {
+      expect(calculator.div(2, 1)).toBe(2);
+      expect(calculator.div(6, -3)).toBe(-2);
+      expect(calculator.div(-4, -8)).toBe("0.5");
+    });
   });
 });
